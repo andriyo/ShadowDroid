729fcc432fc2cebefcc61ad2de5391e7f6e5a6f3
Andrii <andriyo@gmail.com>
2026-06-07 15:57:40 -0700

    Add Android Studio plugin scaffold

---
+++ shadowdroid-plugin/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.intellij.platform")
+}
+
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    testImplementation(libs.junit)
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        androidStudio("2026.1.1.8")
+        testFramework(TestFrameworkType.Platform)
+
+        // Add plugin dependencies for compilation here:
+        bundledPlugin("org.jetbrains.android")
+        bundledPlugin("com.intellij.java")
+        bundledPlugin("org.jetbrains.kotlin")
+    }
+}
