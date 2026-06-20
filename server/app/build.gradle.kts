ee4b2f9a63bcdb81270571481501185d8b32fea4
Andrii <andriyo@gmail.com>
2026-06-19 18:51:29 -0700

    Release v0.4.1

--- server/app/build.gradle.kts
+++ server/app/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 val serverVersionName: String =
     (project.findProperty("version") as? String)
         ?.takeIf { it.isNotBlank() && it != "unspecified" }
-        ?: "0.4.0"
+        ?: "0.4.1"
 
 android {
     namespace = "io.github.andriyo.shadowdroid"
